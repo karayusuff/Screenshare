bce9461f64b550bdd829586d3f141fc8bbccfc26
Yusuf Kara <yusufkara5562@gmail.com>
2025-01-14 18:13:59 +0300

    Add more endpoints to list_routes

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -13,3 +13,45 @@ def get_all_lists():
     """
     lists = List.query.order_by(List.created_at.desc()).all()
     return jsonify({"Lists": [list.to_dict() for list in lists]}), 200
+
+
+@list_routes.route('/<int:list_id>', methods=['PUT'])
+@login_required
+def update_list(list_id):
+    """
+    Updates a list belogs to the current user.
+    """
+    list = List.query.get(list_id)
+    if not list:
+        return jsonify({"error": "List not found."}), 404
+    
+    if current_user.id != list.user_id:
+        return jsonify({"error": "Unauthorized."}), 401
+
+    form = ListForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        list.name = form.name.data or list.name
+
+        db.session.commit()
+        return jsonify(list.to_dict()), 200
+    
+    return form.errors, 400
+
+
+@list_routes.route('/<int:list_id>', methods=['DELETE'])
+@login_required
+def delete_list(list_id):
+    """
+    Deletes a list belogs to the current user.
+    """
+    list = List.query.get(list_id)
+    if not list:
+        return jsonify({"error": "List not found."}), 404
+    
+    if current_user.id != list.user_id:
+        return jsonify({"error": "Unauthorized."}), 401
+    
+    db.session.delete(list)
+    db.session.commit()
+    return jsonify({"message": "List deleted successfully."}), 200
