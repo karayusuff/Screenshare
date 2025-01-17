5fdfd6c63dbd6af969576224054e482b2b50992c
Yusuf Kara <yusufkara5562@gmail.com>
2025-01-17 04:08:18 +0300

    Update nav-bar

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,29 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import logo from "../../../../images/screenshare-logo-transparent.png"
 import "./Navigation.css";
 
 function Navigation() {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+    <nav className="nav-bar">
+      <div className="nav-left">
+        <NavLink to="/">
+          <img src={logo} alt="Screenshare Logo" title="Lights, Camera, Socialize!" className="nav-logo" />
+        </NavLink>
+      </div>
 
-      <li>
+      <div className="nav-center">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="nav-search-bar"
+        />
+      </div>
+
+      <div className="nav-right">
         <ProfileButton />
-      </li>
-    </ul>
+      </div>
+    </nav>
   );
 }
 
