6a1afb4de424d25c020a8af16a7d6b0e9b1fbea7
Yusuf Kara <yusufkara5562@gmail.com>
2025-03-01 17:38:05 -0500

    Update seed data of the lists

--- app/seeds/lists.py
+++ app/seeds/lists.py
@@ -3,31 +3,29 @@ from sqlalchemy.sql import text
 
 def seed_lists():
     seed_data = [
-        List(
-            user_id=2,
-            name="Favourites",
-            list_type="Default"
-        ),
-        List(
-            user_id=2,
-            name="Watchlist",
-            list_type="Default"
-        ),
-        List(
-            user_id=3,
-            name="Watched",
-            list_type="Default"
-        ),
-        List(
-            user_id=2,
-            name="Best Action Movies",
-            list_type="Custom"
-        ),
-        List(
-            user_id=2,
-            name="Comfort Movies",
-            list_type="Custom"
-        )
+        List(user_id=2, name="Favourites", list_type="Default"),
+        List(user_id=2, name="Watchlist", list_type="Default"),
+        List(user_id=2, name="Watched", list_type="Default"),
+
+        List(user_id=3, name="Favourites", list_type="Default"),
+        List(user_id=3, name="Watchlist", list_type="Default"),
+        List(user_id=3, name="Watched", list_type="Default"),
+
+        List(user_id=4, name="Favourites", list_type="Default"),
+        List(user_id=4, name="Watchlist", list_type="Default"),
+        List(user_id=4, name="Watched", list_type="Default"),
+
+        List(user_id=5, name="Favourites", list_type="Default"),
+        List(user_id=5, name="Watchlist", list_type="Default"),
+        List(user_id=5, name="Watched", list_type="Default"),
+
+        List(user_id=6, name="Favourites", list_type="Default"),
+        List(user_id=6, name="Watchlist", list_type="Default"),
+        List(user_id=6, name="Watched", list_type="Default"),
+
+
+        List(user_id=2, name="Best Action Movies", list_type="Custom"),
+        List(user_id=2, name="Comfort Movies", list_type="Custom")
     ]
 
     db.session.bulk_save_objects(seed_data)
@@ -38,5 +36,4 @@ def undo_lists():
         db.session.execute(f"TRUNCATE table {SCHEMA}.lists RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM lists"))
-        
     db.session.commit()
